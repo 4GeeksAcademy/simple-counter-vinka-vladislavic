f5d0e92d5f9b97f3f9f7db0eec817d2d47d5ad84
Vinka Vladislavic <123342469+VinkaVladislavic@users.noreply.github.com>
2024-09-21 04:58:08 +0000

    simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,40 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
 //create your first component
-const Home = () => {
+
+const Counter = () => {
+	const [count, setCount] = useState(0)
+
+	useEffect(() => {
+		let intervalId = setInterval(() => {
+			setCount(count => count + 1)
+		},1000);
+		return () => {
+			clearInterval(intervalId)
+		}
+	},[count])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="counter-container">
+			<div className="counter-container-element">
+				<i className="bi bi-clock"></i>
+			</div>
+			<div className="counter-container-element"><p>{(Math.floor(count/100000))%10}</p></div>
+			<div className="counter-container-element"><p>{(Math.floor(count/10000))%10}</p></div>
+			<div className="counter-container-element"><p>{(Math.floor(count/1000))%10}</p></div>
+			<div className="counter-container-element"><p>{(Math.floor(count/100))%10}</p></div>
+			<div className="counter-container-element"><p>{(Math.floor(count/10))%10}</p></div>
+			<div className="counter-container-element"><p>{count%10}</p></div>
 		</div>
+	)
+
+}
+
+const Home = () => {
+	
+	return (
+	
+		<Counter/>
 	);
 };
 
